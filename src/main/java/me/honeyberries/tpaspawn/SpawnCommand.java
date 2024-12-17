65513155279f37401d1fa40cfed9869ac98d7cca
Henry-New-Account <henry.rainbowfish@gmail.com>
2024-12-16 22:46:12 -0800

    Initial commit

---
+++ src/main/java/me/honeyberries/tpaspawn/SpawnCommand.java
@@ -0,0 +1,41 @@
+package me.honeyberries.tpaspawn;
+
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class SpawnCommand implements CommandExecutor, TabExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage("Only players can use this command! Sorry");
+
+            return true;
+        }
+
+        World world = getServer().getWorld("world");
+        Player player = (Player) commandSender;
+
+        player.teleport(world.getSpawnLocation());
+        commandSender.sendMessage("You went to the world's spawn!");
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return new ArrayList<>();
+    }
+}
